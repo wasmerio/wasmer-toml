f0ba2297472e4be3ada4b69e27a452e70f0b8629
Felix Schütt <felix@wasmer.io>
2022-11-21 15:18:55 +0100

    Add Wapm struct

--- src/lib.rs
+++ src/lib.rs
@@ -660,6 +660,18 @@ pub enum ValidationError {
     MissingModuleForCommand(String, String),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Wapm {
+    pub namespace: String,
+    pub package: Option<String>,
+    pub wasmer_extra_flags: Option<String>,
+    pub abi: Abi,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub fs: Option<HashMap<String, PathBuf>>,
+    pub bindings: Option<Bindings>,
+}
+
 #[cfg(test)]
 mod serialization_tests {
     use super::*;
